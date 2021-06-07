1778a1eef971fd24d0e10d2ce9853101d6870b92
RAHULK2308 <rahulkm4002@gmail.com>
2021-06-07 21:15:34 +0530

    message

--- src/routes/userrouter.js
+++ src/routes/userrouter.js
@@ -3,10 +3,7 @@ const userRouter=express.Router();
 const userdata=require('../model/userdata');
 userRouter.use(express.urlencoded({extended:true}))
 
-function checkUserAuth(req, res, next) {
-    if (req.session.username) return next();
-    return next(new NotAuthorizedError());
-  }
+
 
 function Router(nav,nav1,nav2){
 
@@ -49,7 +46,7 @@ userRouter.post('/add', function(req,res){
 })
 
 
-userRouter.post('/check', checkUserAuth, function(req,res){
+userRouter.post('/check',  function(req,res){
  
 
     var useremail=req.body.email;
