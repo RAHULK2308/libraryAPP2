555ddab33655a75c063479161a4ec8ca4603497d
RAHULK2308 <rahulkm4002@gmail.com>
2021-06-08 13:51:39 +0530

    message

--- src/model/userdata.js
+++ src/model/userdata.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect('mongodb://localhost:27017/library',{ useNewUrlParser: true,useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://RAHULK:rahul2308@cluster0.72kyu.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false); 
 
 const Schema=mongoose.Schema;
